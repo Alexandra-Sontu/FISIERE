c6147d7caf454f96d32c571e6d787ae663033429
Alexandra-Sontu <71020688+Alexandra-Sontu@users.noreply.github.com>
2021-11-30 20:41:42 +0200

    Add files via upload

---
+++ problema 5 pagina 77.py
@@ -0,0 +1,21 @@
+f=open("C:\Informatica fisiere.txt",'w')
+c=str(input('Introduceti sirul'))
+f.write(c)
+f.close()
+
+f=open("C:\Informatica fisiere.txt",'r')
+cr=f.read()
+print("Sirul:",cr)
+f.close()
+
+v=[]
+for i in range(0,len(cr)):
+    if cr[i]=='a' or cr[i]=='i' or cr[i]=='e' or cr[i]=='o' or cr[i]=='u' or cr[i]=='A' or cr[i]=='E' or cr[i]=='I' or cr[i]=='U' or cr[i]=='O':
+      v.extend(cr[i])
+
+f=open("C:\Informatica vocale.txt",'w')
+f.write(str(v))
+f.write("\n ")
+f.write(str(len(v)))
+f.write("vocale")
+f.close()
